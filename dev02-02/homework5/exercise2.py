00a324ea3cd0bcc3fa0799e24e671fbddd282274
Oscar druivenetak <oscar_rank_46@hotmail.com>
2017-01-03 21:40:54 +0100

    finished assignment 5

--- dev02-02/homework5/exercise2.py
+++ dev02-02/homework5/exercise2.py
@@ -3,59 +3,54 @@
 class Empty(object):
     def __init__(self):
         super(Empty, self).__init__()
-        self.isEmpty = True
+    def __str__(self): return "[]"
+    def isEmpty(self): return True
+    def Length(self): return 0
+    def Sum(self): return 0
+    def Map(self, fn): return self
+    def Filter(self, p): return self
+    def Fold(self, f, z): return z
 
 class Node(object):
     def __init__(self, head, tail):
         super(Node, self).__init__()
-        self.isEmpty = False
-        self.Head = head
-        self.Tail = tail
-
-
+        self.head = head
+        self.tail = tail
+    def __str__(self):
+        return str(self.head) + " << " + str(self.tail)
+    def IsEmpty(self): return False
+    def Head(self): return self.head
+    def Tail(self): return self.tail
+    def Sum(self):
+        return self.head + self.tail.Sum()
+    def Length(self):
+        return 1 + self.tail.Length()
+    def Map(self, fn):
+        return Node(fn(self.head), self.tail.Map(fn))
+    def Filter(self, p):
+        l = self.tail.Filter(p)
+        if p(self.head):
+            return Node(self.head, l)
+        else:
+            return l
+    def Fold(self, f, z):
+        return f(self.head, self.tail.Fold(f,z))
 
 Empty = Empty()
 
-def isEmpty(li):
-    if(li.isEmpty):
-        return True
-    else:
-        return False
+l = Node(5, Node(9, Node(-1, Empty)))
 
-def length(l):
-    global lng
-    def calc(li):
-        if(li.isEmpty):
-            return Empty
-        else:
-            lng = lng + 1
-            return calc(li.Tail)
-    calc(l)
+#exercise 2
+empty = l.IsEmpty()
+ln = l.Length()
+sm = l.Sum()
+print("exercise 2 List results \n isEmpty: {0} \n Length: {1} \n Sum: {2} \n".format(empty, ln, sm))
 
-def map(li, fn):
-    le = len(li)
-    x = 0
-    ret = []
-    while x < le:
-        ret.append(fn(li[x], x))
-        x = x + 1
-    return ret
+#exercise 3
+mp = l.Map(lambda x:  x + 2)
+fl = l.Filter(lambda x: x % 3 == 0)
+print("exercise 3 List results \n Map: {0} \n Filter: {1}".format(mp, fl))
 
-def filter(li, fn):
-    le = len(li)
-    x = 0
-    ret = []
-    while x < le:
-        if(fn(li[x])):
-            ret.append(li[x])
-        x = x + 1
-    return ret
-l = Node(1, Node(2, Empty))
-li = [1,2,3,4]
-
-res = map(li, lambda val, idx:  val * 1)
-print(res)
-
-res = filter(li, lambda val: val % 2 == 0)
-
-print(res)
+#exercise 4
+fold = l.Fold(lambda x, y: x+y, 0)
+print(fold)
